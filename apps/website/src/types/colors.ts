bc67be0618fe3b5ecf0966e2bc644f45130c87ee
Filip Cerny <filip.cerny2@ceskatelevize.cz>
2024-11-30 23:13:36 +0100

    feat(header): add logo as heading

--- apps/website/src/types/colors.ts
+++ apps/website/src/types/colors.ts
@@ -1,1 +1,10 @@
-export type Color = 'red' | 'blue' | 'green';
+export enum Color {
+    green = '#39FF14',
+    grey = '#2C2F33',
+    black = '#0A0A0A',
+    blue = '#00D9FF',
+    pink = '#FF007F',
+    red = '#D00000',
+    yellow = '#FFD700',
+    violet = '#4B0082'
+}
