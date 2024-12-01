a834a1f3780c76e952d06fb8cee7048f7a1c066a
Filip Cerny <filip.cerny2@ceskatelevize.cz>
2024-12-01 21:38:47 +0100

    feat(footer): basic footer implementation

--- apps/website/src/components/Footer/Footer.tsx
+++ apps/website/src/components/Footer/Footer.tsx
@@ -1,11 +1,10 @@
+import { Text } from '../Text/Text';
+import styles from './Footer.module.scss';
+
 export const Footer = () => {
   return (
-    <div>
-      <p>Copyright 2024 Blich Game Studio. All rights reserved.</p>
-      <ul>
-        <li><a href="#">Privacy Policy</a></li>
-        <li><a href="#">Terms & Conditions</a></li>
-      </ul>
+    <div className={styles.footer}>
+      <Text align='center' as='p'>© 2024 Blich Game Studio. All rights reserved.</Text>
     </div>
   )
 }
