b7577c0a9e469b3a27f6a326dd8b943f6a2aedb6
f33koslav <f33koslav@gmail.com>
2024-12-14 20:47:17 +0100

    feat: fixed footer

--- apps/website/src/components/Footer/Footer.tsx
+++ apps/website/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
   return (
     <div className={styles.footer}>
-      <Text align='center' as='p'>© 2024 Blich Game Studio. All rights blablabla reserved. Cus blabla</Text>
+      <Text align='center' as='p'>© 2024 Blich Game Studio. All rights blablabla reserved.</Text>
     </div>
   )
 }
